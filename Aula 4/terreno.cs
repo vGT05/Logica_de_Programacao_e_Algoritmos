5afa8032ac0e5a78d4256bad07cfadfcceb6a351
vGT05 <roserianriva090@gmail.com>
2026-02-18 21:58:23 -0300

    terreno

--- Aula 4/terreno.cs
+++ Aula 4/terreno.cs
@@ -4,12 +4,16 @@ using static System.Console;
 double comp, larg, area, preco;
 
 
-System.Console.WriteLine("Digite o comprimento do terreno: ");
+System.Console.WriteLine("Digite o comprimento do terreno: ");//entrada de dados
 comp = double.Parse(ReadLine());
-System.Console.WriteLine("Digite a largura do terreno: ");
+
+System.Console.WriteLine("Digite a largura do terreno: ");//entrada de dados
 larg = double.Parse(ReadLine());
-area = comp*larg;
-System.Console.WriteLine($"a área do terreno é {area}");
-System.Console.WriteLine("Digite o valor do m² do terreno: ");
+
+area = comp*larg;//processamento
+System.Console.WriteLine($"a área do terreno é {area}");//saida
+
+System.Console.WriteLine("Digite o valor do m² do terreno: ");//entrada
 preco = double.Parse(ReadLine());
-System.Console.WriteLine($"O valor do terreno é {area*preco}");
+
+System.Console.WriteLine($"O valor do terreno é {area*preco}");// processamento + saida
