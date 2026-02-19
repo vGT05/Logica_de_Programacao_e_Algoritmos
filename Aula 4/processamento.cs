ee3cd8f752226a1023feac13fd977a1d884f19d9
vGT05 <roserianriva090@gmail.com>
2026-02-18 21:08:31 -0300

    AULA4

---
+++ Aula 4/processamento.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+//Processamento de dados
+//Operações Aritiméticas
+System.Console.WriteLine(1+2);//Soma
+System.Console.WriteLine(7-3);//Subtração
+System.Console.WriteLine(5*3);//Multiplicação
+System.Console.WriteLine(3/2);//Divisão
+WriteLine(7%3);//Resto da divisão
+WriteLine(2*2*2);//Potenciação
+WriteLine(2+2/2);
---
+++ Aula 4/saida.cs
@@ -0,0 +1,14 @@
+
+string nome;//criando a variavel
+nome = "Clodoaldo";//Variavel do tipo string
+System.Console.WriteLine(nome);//saida de dados com quebra de linha
+System.Console.Write(nome);//saida de dados sem quebra de linha
+
+const string GATOBURRO = "laranja";//Criando Variavel do tipo Constante
+System.Console.WriteLine(GATOBURRO);
+string nome2;
+nome2 = "Ronaldo";
+System.Console.WriteLine("O nome do usuário é " + nome2);//concatenacao
+System.Console.WriteLine($"O gato {GATOBURRO} é muito burro");//interpolacao
+
+
