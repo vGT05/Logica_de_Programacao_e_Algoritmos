779f6073802daa97bfb5fdf0d5b664c6fbd2e295
vGT05 <roserianriva090@gmail.com>
2026-02-23 19:37:41 -0300

    com try e catch

--- Aula 6/medidas.cs
+++ Aula 6/medidas.cs
@@ -1,18 +1,23 @@
 using static System.Console;
+try{
+    double a, b, c, areaQ, areaTri, AreaTrap;
 
-double a, b, c, areaQ, areaTri, AreaTrap;
+    WriteLine("Digite a medida A: ");
+    a = double.Parse(ReadLine());
+    WriteLine("Digite a medida B: ");
+    b = double.Parse(ReadLine());
+    WriteLine("Digite a medida C: ");
+    c = double.Parse(ReadLine());
 
-WriteLine("Digite a medida A: ");
-a = double.Parse(ReadLine());
-WriteLine("Digite a medida B: ");
-b = double.Parse(ReadLine());
-WriteLine("Digite a medida C: ");
-c = double.Parse(ReadLine());
+    areaQ = a*a;
+    areaTri = (a*b)/2;
+    AreaTrap = ((a+b)*c)/2;
 
-areaQ = a*a;
-areaTri = (a*b)/2;
-AreaTrap = ((a+b)*c)/2;
-
-WriteLine($"A área do quadrado é {areaQ:F4}");
-WriteLine($"A área do triangulo é {areaTri:F4}");
-WriteLine($"A área do trapézio é {AreaTrap:F4}");
+    WriteLine($"A área do quadrado é {areaQ:F4}");
+    WriteLine($"A área do triangulo é {areaTri:F4}");
+    WriteLine($"A área do trapézio é {AreaTrap:F4}");
+}
+catch(Exception)
+{
+    WriteLine("Valores invalidos inseridos, programa encerrado.");
+}
