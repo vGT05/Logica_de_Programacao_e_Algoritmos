4b8b8ad08c235d35459f4805a47dda7db87a03cf
vGT05 <roserianriva090@gmail.com>
2026-02-23 21:45:23 -0300

    Comparadores

--- Aula 6/comparadorSupremo.cs
+++ Aula 6/comparadorSupremo.cs
@@ -18,7 +18,7 @@ WriteLine($"1°: O primeiro valor é igual ao terceiro valor? {a1}\n");
 WriteLine($"2°: O segundo valor é diferente do primeiro valor? {a2}\n");
 WriteLine($"3°: O segundo valor é maior que o terceiro valor? {a3}\n");
 WriteLine($"4°: O primeiro valor é menor ou igual que o terceiro valor? {a4}\n");
-
+WriteLine("-----------------------------------------------------------\n");
 string pr = a1 & a3 ? "São verdadeiras":"São falsas";
 string sg = a2 | a4 ? "Uma das respostas é verdadeira":"Nenhuma das respostas é verdadeira";
 string tr = !a1 == true ? "A negação é verdadeira":"A negação é falsa";
