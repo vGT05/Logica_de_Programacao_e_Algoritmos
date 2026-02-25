4b45465dadbeaf9eeed012ad303ab449f7457c15
vGT05 <roserianriva090@gmail.com>
2026-02-25 18:43:39 -0300

    Comparadores

--- Aula 6/comparadorSupremo.cs
+++ Aula 6/comparadorSupremo.cs
@@ -8,6 +8,7 @@ Write("Digite o segundo valor: ");
 b = int.Parse(ReadLine());
 Write("Digite o terceiro valor: ");
 c = int.Parse(ReadLine());
+WriteLine("\n");
 
 bool a1 = a == c;
 bool a2 = b != a;
@@ -19,7 +20,7 @@ WriteLine($"2°: O segundo valor é diferente do primeiro valor? {a2}\n");
 WriteLine($"3°: O segundo valor é maior que o terceiro valor? {a3}\n");
 WriteLine($"4°: O primeiro valor é menor ou igual que o terceiro valor? {a4}\n");
 WriteLine("-----------------------------------------------------------\n");
-string pr = a1 & a3 ? "São verdadeiras":"São falsas";
+string pr = a1 & a3 ? "São verdadeiras":"Não";
 string sg = a2 | a4 ? "Uma das respostas é verdadeira":"Nenhuma das respostas é verdadeira";
 string tr = !a1 == true ? "A negação é verdadeira":"A negação é falsa";
 
