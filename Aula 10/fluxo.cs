db7123acaa53f7a4dc5c76a4b876c4f93ebe1aee
vGT05 <roserianriva090@gmail.com>
2026-03-05 18:25:37 -0300

    fluxo

--- Aula 10/fluxo.cs
+++ Aula 10/fluxo.cs
@@ -1,40 +1,54 @@
-using System.Runtime.Serialization;
 using static System.Console;
 DateTime data = DateTime.Now;
 
 string entrada;
-int turistas=0, total=0, contagem;
+int turistas=0, contagem = 0;
 
 WriteLine("Aplicativo de controle de fluxo de carros");
 WriteLine("Parque Nacional os Lençóis Maranhenses\n");
 
 while (true)
 {
-    WriteLine("Digite o fluxo de carro (entrada/saida) ou sair para encerrar a aplicação: ");
+    Write("Digite o fluxo de carro (entrada/saida) ou sair para encerrar a aplicação: ");
     entrada = ReadLine();
     
     if (entrada == "entrada")
     {
         Write("Digite o número de turistas: ");
         turistas = int.Parse(ReadLine());
-        contagem = total + turistas;
-        WriteLine($"Entrada de turistas registrada em: {data:dd/MM;yyyy hh:mm:ss}");
-        WriteLine($"Número de turistas no parque: {turistas}");
+            if (turistas <= 4)
+        {
+        contagem = contagem + turistas;
+        WriteLine($"Entrada de turistas registrada em: {data:dd/MM/yyyy hh:mm}");
+        WriteLine($"Número de turistas no parque: {contagem}");  
+        }
+            else
+            {
+            WriteLine("Não é possivel registrar esse valor.");
+            }
     }
-    if (entrada == "saida")
+    else if (entrada == "saida")
     {
         Write("Digite o número de turistas: ");
         turistas = int.Parse(ReadLine());
-        contagem = total - turistas;
-        WriteLine($"Saída de turistas registrada em: {data:dd/MM;yyyy hh:mm:ss}");
+            if (turistas <= contagem)
+        {
+        contagem = contagem - turistas;
+        WriteLine($"Saída de turistas registrada em: {data:dd/MM/yyyy hh:mm}");
         WriteLine($"Número de turistas no parque: {contagem}");
+        }   
+            else
+            {
+            WriteLine("Não é possivel registrar esse valor.");
+            }
+    
     }
-    if (entrada == "sair")
+    else if (entrada == "sair")
     {
         WriteLine("Encerrando programa.");
         break;
     }
-    else
+    else if (entrada != "entrada" | entrada != "saida" | entrada != "sair")
     {
         WriteLine("ERRO: Digite um dos comandos citados anteriormente");
     }
